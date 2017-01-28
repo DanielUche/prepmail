8ff681538e254b3b8dd62fc8a75fae60e11913c9
DanielUche <dank.uche@yahoo.com>
2017-01-28 11:52:05 +0100

    first

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+   
+   <head>
+      <title></title>
+   </head>
+   <body>
+   <p>
+   		My experirence with creating wep apps and microservies will enable me fit into the roel of a senior software developer with preplass.
+   </p>
+   	
+   </body>
+   
+   <body>
+      
+      <?php
+         $to = "riomachi@gmail.com";
+         $subject = "This is subject";
+         
+         $message = "<b>This is HTML message.</b>";
+         $message .= "<h1>This is headline.</h1>";
+         
+         $header = "From:dank.uche@yahoo.com \r\n";
+         $header .= "Cc:doctorfox29@yahoo.co.uk \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+      ?>
+      
+   </body>
+</html>
